a15eae0fc9e27e454cab9c520d027e2be89a9fe1
Xuan127 <shanyuxuan2001@gmail.com>
2025-03-22 11:54:34 -0700

    gitignore and apify test script

---
+++ apify_test.py
@@ -0,0 +1,26 @@
+from apify_client import ApifyClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Initialize the ApifyClient with your Apify API token
+# Replace '<YOUR_API_TOKEN>' with your token.
+client = ApifyClient(os.getenv("APIFY_API_TOKEN"))
+
+# Prepare the Actor input
+run_input = {
+    "searchTerms": ["apify"],
+    "sort": "Latest",
+    "maxItems": 1000,
+}
+
+# Run the Actor and wait for it to finish
+run = client.actor("apidojo/twitter-scraper-lite").call(run_input=run_input)
+
+# Fetch and print Actor results from the run's dataset (if there are any)
+print("💾 Check your data here: https://console.apify.com/storage/datasets/" + run["defaultDatasetId"])
+for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+    print(item)
+
+# 📚 Want to learn more 📖? Go to → https://docs.apify.com/api/client/python/docs/quick-start
